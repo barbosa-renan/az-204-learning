d4abc8cb59f15388b5f568d6d4572f0cc9a0204f
Renan Vinícius Quadros Barbosa <renan.q.barbosa@avanade.com>
2024-10-28 17:57:22 -0300

    feat: change authorization level

--- anp-az-dev-function-01/Function01/Function1.cs
+++ anp-az-dev-function-01/Function01/Function1.cs
@@ -16,7 +16,7 @@ namespace Function01
         }
 
         [Function("Function1")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             string name = req.Query["name"];
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
