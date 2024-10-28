bca19bd72c75c6195e5e5c62d9eb3addab0ba009
Renan Vinícius Quadros Barbosa <renan.q.barbosa@avanade.com>
2024-10-28 17:50:41 -0300

    create timer trigger function

---
+++ anp-az-dev-timer-trigger/anp-az-dev-timer-trigger/Function1.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace anp_az_dev_timer_trigger
+{
+    public class Function1
+    {
+        private readonly ILogger _logger;
+
+        public Function1(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Function1>();
+        }
+
+        [Function("Function02")]
+        public void Run([TimerTrigger("0*/1****")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+}
